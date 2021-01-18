85eef39ec453e0a6c4a63f5ed4b59b0338b72b7a
lwildmann <wildmannlukas03@gmail.com>
2021-01-18 10:43:52 +0100

    create randombvalue script

---
+++ Assets/MyGame/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    public float Number;
+    public Text Value;
+
+    public void ValueButton ()
+    {
+        Number = UnityEngine.Random.value;
+        Value.text = Number.ToString();
+        Debug.Log("Ich werde gedrückt.");
+    }
+}
